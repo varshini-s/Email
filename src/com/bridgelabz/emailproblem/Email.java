e6ecebb323f4503f2f6a7f893d7dce359b8693d2
varshini <dreamvarshini@gmail.com>
2021-09-07 19:41:41 +0530

    ADDED: printing welcome message for email problem

---
+++ src/com/bridgelabz/emailproblem/Email.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.emailproblem;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Email 
+{
+
+	public static void main(String[] args) 
+
+	{
+		System.out.println("welcome to email problem");
+		
+	}
+}

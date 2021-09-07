743e44869a1b90ab384e8298e7ed291c1030f54b
varshini <dreamvarshini@gmail.com>
2021-09-07 19:43:35 +0530

    ADDED: list of valid and invalid mail id's

--- src/com/bridgelabz/emailproblem/Email.java
+++ src/com/bridgelabz/emailproblem/Email.java
@@ -10,6 +10,22 @@ public class Email
 
 	{
 		System.out.println("welcome to email problem");
+
+
 		
+
+		String[] validMails = {"abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net",
+				"abc.100@abc.com.au","abc@1.com","abc@gmail.com.com","abc+100@gmail.com"};
+
+		String[] invalidMails= {"abc","abc@.com.my","abc123@gmail.a","abc123@.com.com",".abc@abc.com","abc()*@gmail.com","abc@%*.com","abc..2002@gmail.com"
+				,"abc.@gmail.com","abc@abc@gmail.com","abc@gmail.com.1a",".abc@gmail.com.aa.au"};
+
+
+		int allValidMails=1,allInvalidMails=1;
+
+
+		
+
+
 	}
 }
